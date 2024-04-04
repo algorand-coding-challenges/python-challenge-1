a3a46e31153689b1e827a05dcaa1ef98393a65b4
James Teasley <james.h.teasley@gmail.com>
2024-04-03 21:25:54 -0400

    Fixed the solution for contract.py.

--- projects/challenge/smart_contracts/personal_vault/contract.py
+++ projects/challenge/smart_contracts/personal_vault/contract.py
@@ -23,7 +23,7 @@ class PersonalVault(ARC4Contract):
     def deposit(self, ptxn: gtxn.PaymentTransaction) -> UInt64:
         assert ptxn.amount > 0, "Deposit amount must be greater than 0"
         assert (
-            ptxn.receiver == Global.creator_address
+            ptxn.receiver == Global.current_application_address
         ), "Deposit receiver must be the contract address"
         assert ptxn.sender == Txn.sender, "Deposit sender must be the caller"
         assert op.app_opted_in(
